b5add4523ac4c9febcc38830ef71ee69cec7d913
HarryPopo <41724873+popoopendoor@users.noreply.github.com>
2025-10-17 15:29:20 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -17,7 +17,7 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享技巧,
+  HEO_HERO_TITLE_1: '分享技巧',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'VFXSKILL.COM',
   HEO_HERO_TITLE_4: '素材库上线',
